838c5f1f89092a40b7bd32267375d10ebc92f245
SummerMTech <dan.rcr01@gmail.com>
2022-10-30 20:54:29 -0400

    New files for practice added

---
+++ first/src/main/java/client.java
@@ -0,0 +1,11 @@
+public class client {
+    public static void main(String[] asdad){
+        int a=3;
+        int b=4;
+        if(a>b){
+            System.out.println("EL mayor es "+a);
+        }else{
+            System.out.println("EL mayor es "+b);
+        }
+    }
+}
